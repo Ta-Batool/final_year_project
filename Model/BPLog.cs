13482f7011e0e16240f53376c94fb6b17d8e38d7
Ta-Batool <tayyababatool885@gmail.com>
2025-12-18 21:12:12 +0500

    asd

--- Model/BPLog.cs
+++ Model/BPLog.cs
@@ -12,7 +12,8 @@ namespace Model
         public string UserId { get; set; } = "";
         public int Systolic { get; set; }
         public int Diastolic { get; set; }
-        public int Pulse { get; set; }
+        public int? Pulse { get; set; }
+
         public DateTime LoggedAt { get; set; } = DateTime.UtcNow;
 
         public string? Notes { get; set; }
