4ebb0118d24065d407fb511074b2e04f116de106
Ta-Batool <tayyababatool885@gmail.com>
2025-12-18 23:14:51 +0500

    ;

--- BlazorApp1/Service/HealathApiService.cs
+++ BlazorApp1/Service/HealathApiService.cs
@@ -13,39 +13,46 @@ namespace BlazorApp1.Service
         }
 
         // ---------- BP ----------
-        public async Task<List<BPLog>?> GetBPAsync(string userId)
-        {
-            return await _http.GetFromJsonAsync<List<BPLog>>($"api/health/bp/{userId}");
-        }
+        public async Task<List<BPLog>> GetBPAsync(string userId)
+            => await GetOrThrow<List<BPLog>>($"api/healthlogs/bp/{userId}") ?? new();
 
         public async Task AddBPAsync(BPLog log)
-        {
-            var res = await _http.PostAsJsonAsync("api/health/bp", log);
-            res.EnsureSuccessStatusCode();
-        }
+            => await PostOrThrow("api/healthlogs/bp", log);
 
         // ---------- GLUCOSE ----------
-        public async Task<List<GlucoseLog>?> GetGlucoseAsync(string userId)
-        {
-            return await _http.GetFromJsonAsync<List<GlucoseLog>>($"api/health/glucose/{userId}");
-        }
+        public async Task<List<GlucoseLog>> GetGlucoseAsync(string userId)
+            => await GetOrThrow<List<GlucoseLog>>($"api/healthlogs/glucose/{userId}") ?? new();
 
         public async Task AddGlucoseAsync(GlucoseLog log)
-        {
-            var res = await _http.PostAsJsonAsync("api/health/glucose", log);
-            res.EnsureSuccessStatusCode();
-        }
+            => await PostOrThrow("api/healthlogs/glucose", log);
 
         // ---------- WEIGHT ----------
-        public async Task<List<WeightLog>?> GetWeightAsync(string userId)
-        {
-            return await _http.GetFromJsonAsync<List<WeightLog>>($"api/health/weight/{userId}");
-        }
+        public async Task<List<WeightLog>> GetWeightAsync(string userId)
+            => await GetOrThrow<List<WeightLog>>($"api/healthlogs/weight/{userId}") ?? new();
 
         public async Task AddWeightAsync(WeightLog log)
+            => await PostOrThrow("api/healthlogs/weight", log);
+
+        // ---------- Helpers ----------
+        private async Task<T?> GetOrThrow<T>(string url)
         {
-            var res = await _http.PostAsJsonAsync("api/health/weight", log);
-            res.EnsureSuccessStatusCode();
+            using var res = await _http.GetAsync(url);
+            if (!res.IsSuccessStatusCode)
+            {
+                var msg = await res.Content.ReadAsStringAsync();
+                throw new Exception($"GET {url} failed: {(int)res.StatusCode} {res.ReasonPhrase}. {msg}");
+            }
+            return await res.Content.ReadFromJsonAsync<T>();
+        }
+
+        private async Task PostOrThrow<TBody>(string url, TBody body)
+        {
+            using var res = await _http.PostAsJsonAsync(url, body);
+            if (!res.IsSuccessStatusCode)
+            {
+                var msg = await res.Content.ReadAsStringAsync();
+                throw new Exception($"POST {url} failed: {(int)res.StatusCode} {res.ReasonPhrase}. {msg}");
+            }
         }
     }
 }
